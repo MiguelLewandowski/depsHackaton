3fd2501f2aec065f7827767cc0c244dc06c80c99
MiguelLewandowski <miguelstrapazonl@gmail.com>
2023-10-07 09:05:34 -0300

    a

--- themes/web/home.php
+++ themes/web/home.php
@@ -13,7 +13,7 @@
 </head>
 
 <header class="header container-fluid d-flex align-items-center p-2">
-    <a class="logo mx-5 m-2">River-Care</a>
+    <a class="logo mx-5 m-2">CalamityControl</a>
     <form class="form-inline mx-5 flex-grow-1">
         <input type="text" class="form-control form-control-lg flex-grow-1 mr-2" placeholder="Buscar cidade">
         <button type="submit" class="btn btn-primary">
@@ -45,22 +45,7 @@
                     </h1>
                     <div class="card2 text-danger">
                         <h4>Cidade 1</h4>
-<<<<<<< HEAD:themes/web/home.php
                         <p>Nível de necessidade: 10</p>
-=======
-                        <p>Necessidades: 10</p>
-                    </div>
-                    <div class="card2">
-                        <h4>Cidade 2</h4>
-                        <p>Necessidades: 8</p>
-                    </div>
-                    <div class="card2">
-                        <h4>Cidade 3</h4>
-                        <p>Necessidades: 7</p>
-                    </div>
-                    <div class="card2">
-                        <p>Nível de neceessidade: 10</p>
->>>>>>> 6a7915253b3816ea1afa580a4db62a9e4875f3cf:index.html
                     </div>
                     <div class="card2 text-danger">
                         <h4>Cidade 2</h4>
@@ -76,7 +61,6 @@
                     </div>
                     <div class="card2 text-warning">
                         <h4>Cidade 5</h4>
-                        <p>Necessidades: 4</p>
                         <p>Necessidades: 5</p>
                     </div>
                 </div>
@@ -87,23 +71,8 @@
                 <h1 class="mt-3 text-light text-center m-2">Previsão do Tempo para Porto Alegre</h1>
                 <div id="weather"></div>
             </div>
-<<<<<<< HEAD:themes/web/home.php
-            <div class="previsao mt-5 p-3">
-                <h2 class="text-center text-light">
-                    Notícias
-                </h2>
-                <h3 class="text-light">
-                    Rio Ibirapuitã volta a atingir cota de inundação em Alegrete - 28/09/2023
-                </h3>
-                <h5 class="text-light">Segundo a defesa civil, 266 pessoas continuam fora de casa e ainda não devem retornar.</h5>
-            </div>
-        </div>
-    </div>
-    <script src="<?= url("assets/web/scripts/apiTempo.js"); ?>"></script>
-=======
-            <div class="previsao mt-5">
-                <h2 class="text-center">Notícias</h2>
-                <div class="previsao mt-5 p-3">
+
+            <div class="previsao mt-5 p-2">
                     <h2 class="text-center text-light">
                         Notícias
                     </h2>
@@ -114,8 +83,8 @@
                 </div>
 
             </div>
-            <script async src="apiTempo.js"></script>
->>>>>>> 6a7915253b3816ea1afa580a4db62a9e4875f3cf:index.html
+            <script async src="<?= url("assets/web/scripts/apiTempo.js"); ?>"></script>
+
 </body>
 
 </html>
